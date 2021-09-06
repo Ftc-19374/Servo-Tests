061e1fedf6bb85ee3085dff6329ffe8d3cbe1b87
Jaws-all <77344665+Jaws-all@users.noreply.github.com>
2021-09-06 18:31:39 -0400

    Add files via upload

---
+++ test_servo.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="test_servo")
+public class test_servo extends LinearOpMode{
+
+    CRServo servo1;
+    boolean ButtonA;
+    boolean ButtonB;
+
+    public void runOpMode() throws InterruptedException {
+        servo1 = hardwareMap.crservo.get("servo1");
+        waitForStart();
+        while (opModeIsActive()) {
+            ButtonA = gamepad1.a;
+            ButtonB = gamepad1.b;
+
+            if (ButtonA) {
+                servo1.setPower(1.0);
+            }
+            if (ButtonB) {
+                servo1.setPower(-1.0);
+            }
+        }
+        }
+
+}
